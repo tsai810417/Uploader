e2b11aa95dbc93d7e943347e06297fd8a1da5fb2
Chris Tsai <tsai_cheng@yahoo.com>
2020-01-17 15:05:08 +0800

    include build

---
+++ build/precache-manifest.d4506cb45b4997b22def1670ff85e25b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1721a5d2ba322991680d8d21d0c314a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d41f9dee10a2d3341317",
+    "url": "/static/css/main.f21833ee.chunk.css"
+  },
+  {
+    "revision": "8cfe3320291258c79180",
+    "url": "/static/js/2.a97c734b.chunk.js"
+  },
+  {
+    "revision": "6c19264f77eccd3457f082f79260f8b6",
+    "url": "/static/js/2.a97c734b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d41f9dee10a2d3341317",
+    "url": "/static/js/main.5f9ada29.chunk.js"
+  },
+  {
+    "revision": "3e2764314f5ba17c5a67",
+    "url": "/static/js/runtime-main.93c1fce8.js"
+  },
+  {
+    "revision": "f75dfd4a4970efbd25e81f8e0605299e",
+    "url": "/static/media/diabnext_logo.f75dfd4a.png"
+  }
+]);
